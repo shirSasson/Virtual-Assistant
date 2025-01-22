25e45b68b6fd8f07c1f997b7e5fd79c168bf02a8
shir <shir@sasson.me>
2025-01-22 14:45:37 +0200

    add search controller

---
+++ frontend/src/services/searchController.ts
@@ -0,0 +1,20 @@
+import axios from 'axios';
+
+const API_URL = import.meta.env.VITE_API_URL;
+
+
+export const sendData = async (data: string): Promise<string> => {
+    try {
+        const response = await axios.post(`${API_URL}`, { question: data }, {
+            headers: {
+                'Content-Type': 'application/json'
+            },
+        });
+        return response.data.answer as string;
+    } catch (error) {
+        console.error('Error posting data:', error);
+        throw error;
+    }
+};
+
+export default { sendData };
